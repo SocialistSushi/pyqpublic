10ac2297c87686dcfad06ccce4e922f50013aad6
Bryan L. Fordham <bfordham@gmail.com>
2017-07-06 19:11:45 -0400

    Some refactoring, moving more function in to parent class. Handle loading Parcel data

--- qpublic/Bag.py
+++ qpublic/Bag.py
@@ -1,6 +1,18 @@
-class Bag(object):
-	def __init__(self, data):
-		self.data = data
+from bs4 import BeautifulSoup
+from json import JSONEncoder
+import re
+import requests
+import types
+
+class Bag(JSONEncoder):
+	NONALPHA = re.compile('[^a-zA-Z0-9_ ]')
+	UNDERLINES = re.compile('__+')
+
+	def __init__(self, data={}):
+		self.data = {}
+
+		for key, value in data.iteritems():
+			self.setValue(key, value)
 
 	def __getattr__(self, name):
 		if self.data.has_key(name):
@@ -8,5 +20,31 @@ class Bag(object):
 		else:
 			raise AttributeError
 
+	def setValue(self, key, value):
+		if type(value) == types.StringType or type(value) == types.UnicodeType:
+			value = self.normalizeText(value)
+		self.data[self.normalizeKey(key)] = value
+
+
 	def normalizeText(self, text):
 		return ' '.join(text.split())
+
+	def normalizeKey(self, text):
+		key = '_'.join(self.NONALPHA.sub('_', text.lower().replace('-', ' ')).split())
+		return self.UNDERLINES.sub('_', key)
+
+	def load(self):
+		if hasattr(self, 'url') and self.url and not self.loaded:
+			url = 'https://qpublic.schneidercorp.com' + self.url
+			r = requests.get(url)
+			soup = BeautifulSoup(r.text, 'html.parser')
+			self.__init__(soup)
+
+	def default(self):
+		return self.data
+
+	def __str__(self):
+		return str(self.data)
+
+	def __repr__(self):
+		return self.__str__()
--- qpublic/Parcel.py
+++ qpublic/Parcel.py
@@ -3,18 +3,53 @@ from .Bag import Bag
 class Parcel(Bag):
 	def __init__(self, soup):
 		self.url = ''
-		self.notLoaded = False
+		self.loaded = False
+
+		super(Parcel, self).__init__()
 		
 		if soup.name == 'tr':
 			tds = soup.findAll('td')
 			self.url = tds[1].a['href']
-			
-			super(Parcel, self).__init__({
-				'parcelId': self.normalizeText(tds[1].text),
-				'alternateId': self.normalizeText(tds[2].text),
-				'owner': self.normalizeText(tds[3].text),
-				'propertyAddress': self.normalizeText(tds[4].text),
-				'legalDescription': self.normalizeText(tds[5].text)
-				})
-		else:
-			pass
+
+			self.setValue('parcelId', tds[1].text)
+			self.setValue('alternateId', tds[2].text)
+			self.setValue('owner', tds[3].text)
+			self.setValue('propertyAddress', tds[4].text)
+			self.setValue('legalDescription', tds[5].text)
+
+		else: 
+			self.loaded = True
+			for section in soup.findAll('section'):
+				# get title
+				title = section.header.select('span.title')[0].text
+
+				if section.table:
+					if 'tabular-data-two-column' in section.table['class']:
+						subData = {}
+						for row in section.table.findAll('tr'):
+							tds = row.findAll('td')
+							subData[tds[0].text] = tds[1].text
+						self.setValue(title, Bag(subData))
+					elif 'tabular-data' in section.table['class']:
+						subData = []
+						# get column names
+						keys = []
+						for th in section.table.thead.tr.findAll('th'):
+							keys.append(th.text)
+
+						# get data
+						for row in section.table.tbody.findAll('tr'):
+							values = []
+							for td in row.findAll('td'):
+								values.append(td.text)
+							subData.append(Bag({k: v for k, v in zip(keys, values)}))
+						self.setValue(title, subData)
+					elif title =='Owner':
+						subData = Bag()
+						subData.setValue('name', section.select('#ctlBodyPane_ctl01_ctl01_lnkOwnerName_lblSearch'))
+						address = section.select('#ctlBodyPane_ctl01_ctl01_lblAddress') ", " + section.select('#ctlBodyPane_ctl01_ctl01_lblCityStateZip')
+						subData.setValue('address', address)
+
+						self.setValue('owner', subData)
+				else:
+						pass
